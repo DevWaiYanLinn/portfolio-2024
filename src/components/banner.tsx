2cfb4478da55cff9a3a25f19887c991279fe062b
wyl <waiyanlinn.lion@outlook.com>
2024-10-23 07:48:44 +0700

    fix-typo

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,85 +1,77 @@
-import { component$ } from '@builder.io/qwik';
+import { component$ } from "@builder.io/qwik";
 export default component$(() => {
-    return (
-        <div class="absolute top-0 left-0 bottom-0 right-0 flex justify-center items-center flex-col">
-            <h2 class="text-[2.5rem] md:text-[3.5rem] kaushan-script-regular">
-                Hey! I am Clover
-            </h2>
-            <p class="text-md md:text-2xl text-slate-600 poppins-light text-center">
-                A freelance <strong class="text-black">web developer</strong>
-                &nbsp; based in myanamar
-            </p>
-            <p class="text-md md:text-2xl text-slate-600 poppins-light">
-                Turning your <strong class="text-black">ideas</strong> into
-                &nbsp;
-                <strong class="text-black">reality</strong>
-            </p>
-            <div class="flex space-x-7 mt-6">
-                <a href="https://twitter.com/yan_wai71169">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        id="X-Twitter-Logo--Streamline-Logos"
-                        height="20"
-                        width="20"
-                    >
-                        <desc>
-                            X Twitter Logo Streamline Icon:
-                            https://streamlinehq.com
-                        </desc>
-                        <path
-                            fill="#000000"
-                            fill-rule="evenodd"
-                            d="m13.458 9.12244 7.5158 -7.9657h2.4916l-0.0107 0.01176 -8.8892 9.424 8.1385 10.8018c0.2068 0.2744 0.2405 0.6422 0.0872 0.9498 -0.1435 0.2878 -0.4278 0.4764 -0.7453 0.4994h-5.0964c-0.2598 -0.0188 -0.5001 -0.1488 -0.6582 -0.3585l-5.7269 -7.6011 -7.47199 7.9596H0.534546l8.922324 -9.4297L1.31843 2.61205c-0.20678 -0.27444 -0.24055 -0.64223 -0.08721 -0.94974 0.15333 -0.30752 0.4674 -0.50186 0.81102 -0.50186h4.96503c0.28455 0 0.55258 0.13365 0.72381 0.36092L13.458 9.12244Zm-0.7628 1.99966c-0.0257 -0.0299 -0.0491 -0.0611 -0.0703 -0.0934L6.55538 2.97297H3.85973L17.467 21.0334h2.6957l-7.4675 -9.9113Z"
-                            clip-rule="evenodd"
-                            stroke-width="1"
-                        ></path>
-                    </svg>
-                </a>
-                <a href="https://github.com/DevWaiYanLinn">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        id="Github-Logo-2--Streamline-Logos"
-                        height="20"
-                        width="20"
-                    >
-                        <desc>
-                            Github Logo 2 Streamline Icon:
-                            https://streamlinehq.com
-                        </desc>
-                        <path
-                            fill="#000000"
-                            d="M11.996 1.284a10.986 10.986 0 0 0 -3.472 21.412c0.548 0.095 0.722 -0.227 0.722 -0.517 0 -0.263 0.006 -0.991 0 -1.91 -3.057 0.662 -3.688 -1.448 -3.688 -1.448a2.907 2.907 0 0 0 -1.22 -1.607c-0.997 -0.682 0.075 -0.669 0.075 -0.669a2.307 2.307 0 0 1 1.683 1.131 2.34 2.34 0 0 0 3.197 0.914 2.34 2.34 0 0 1 0.697 -1.464c-2.439 -0.279 -5.004 -1.22 -5.004 -5.432a4.248 4.248 0 0 1 1.132 -2.948 3.942 3.942 0 0 1 0.107 -2.907s0.924 -0.295 3.02 1.128a10.402 10.402 0 0 1 5.503 0c2.102 -1.422 3.018 -1.128 3.018 -1.128 0.405 0.92 0.444 1.96 0.109 2.907a4.243 4.243 0 0 1 1.13 2.95c0 4.223 -2.569 5.15 -5.016 5.42a2.604 2.604 0 0 1 0.752 2.026v3.041c0 0.294 0.177 0.619 0.735 0.512a10.986 10.986 0 0 0 -3.48 -21.411Z"
-                            stroke-width="1"
-                        ></path>
-                    </svg>
-                </a>
-                <a href="https://www.linkedin.com/in/wai-yan-lin-809998260/">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        id="Linkedin-Logo--Streamline-Logos"
-                        height="20"
-                        width="20"
-                    >
-                        <desc>
-                            Linkedin Logo Streamline Icon:
-                            https://streamlinehq.com
-                        </desc>
-                        <path
-                            fill="#000000"
-                            fill-rule="evenodd"
-                            d="M3.5 6a2.5 2.5 0 1 0 0 -5 2.5 2.5 0 0 0 0 5ZM6 23V8H1v15h5ZM8 8h4.5v1.946C13.216 9.005 14.746 8 17.5 8c4.33 0 5.5 4.32 5.5 7v8h-5v-8c0 -1 -0.5 -3 -2.5 -3 -1.42 0 -2.42 1.008 -3 1.951V23H8V8Z"
-                            clip-rule="evenodd"
-                            stroke-width="1"
-                        ></path>
-                    </svg>
-                </a>
-            </div>
-        </div>
-    );
+  return (
+    <div class="absolute top-0 left-0 bottom-0 right-0 flex justify-center items-center flex-col">
+      <h2 class="text-[2.5rem] md:text-[3.5rem] kaushan-script-regular">
+        Hey! I am Clover
+      </h2>
+      <p class="text-md md:text-2xl text-slate-600 poppins-light text-center">
+        A freelance <strong class="text-black">web developer</strong>
+        &nbsp; based in Myanmar
+      </p>
+      <p class="text-md md:text-2xl text-slate-600 poppins-light">
+        Turning your <strong class="text-black">ideas</strong> into &nbsp;
+        <strong class="text-black">reality</strong>
+      </p>
+      <div class="flex space-x-7 mt-6">
+        <a href="https://twitter.com/yan_wai71169">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            id="X-Twitter-Logo--Streamline-Logos"
+            height="20"
+            width="20"
+          >
+            <desc>
+              X Twitter Logo Streamline Icon: https://streamlinehq.com
+            </desc>
+            <path
+              fill="#000000"
+              fill-rule="evenodd"
+              d="m13.458 9.12244 7.5158 -7.9657h2.4916l-0.0107 0.01176 -8.8892 9.424 8.1385 10.8018c0.2068 0.2744 0.2405 0.6422 0.0872 0.9498 -0.1435 0.2878 -0.4278 0.4764 -0.7453 0.4994h-5.0964c-0.2598 -0.0188 -0.5001 -0.1488 -0.6582 -0.3585l-5.7269 -7.6011 -7.47199 7.9596H0.534546l8.922324 -9.4297L1.31843 2.61205c-0.20678 -0.27444 -0.24055 -0.64223 -0.08721 -0.94974 0.15333 -0.30752 0.4674 -0.50186 0.81102 -0.50186h4.96503c0.28455 0 0.55258 0.13365 0.72381 0.36092L13.458 9.12244Zm-0.7628 1.99966c-0.0257 -0.0299 -0.0491 -0.0611 -0.0703 -0.0934L6.55538 2.97297H3.85973L17.467 21.0334h2.6957l-7.4675 -9.9113Z"
+              clip-rule="evenodd"
+              stroke-width="1"
+            ></path>
+          </svg>
+        </a>
+        <a href="https://github.com/DevWaiYanLinn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            id="Github-Logo-2--Streamline-Logos"
+            height="20"
+            width="20"
+          >
+            <desc>Github Logo 2 Streamline Icon: https://streamlinehq.com</desc>
+            <path
+              fill="#000000"
+              d="M11.996 1.284a10.986 10.986 0 0 0 -3.472 21.412c0.548 0.095 0.722 -0.227 0.722 -0.517 0 -0.263 0.006 -0.991 0 -1.91 -3.057 0.662 -3.688 -1.448 -3.688 -1.448a2.907 2.907 0 0 0 -1.22 -1.607c-0.997 -0.682 0.075 -0.669 0.075 -0.669a2.307 2.307 0 0 1 1.683 1.131 2.34 2.34 0 0 0 3.197 0.914 2.34 2.34 0 0 1 0.697 -1.464c-2.439 -0.279 -5.004 -1.22 -5.004 -5.432a4.248 4.248 0 0 1 1.132 -2.948 3.942 3.942 0 0 1 0.107 -2.907s0.924 -0.295 3.02 1.128a10.402 10.402 0 0 1 5.503 0c2.102 -1.422 3.018 -1.128 3.018 -1.128 0.405 0.92 0.444 1.96 0.109 2.907a4.243 4.243 0 0 1 1.13 2.95c0 4.223 -2.569 5.15 -5.016 5.42a2.604 2.604 0 0 1 0.752 2.026v3.041c0 0.294 0.177 0.619 0.735 0.512a10.986 10.986 0 0 0 -3.48 -21.411Z"
+              stroke-width="1"
+            ></path>
+          </svg>
+        </a>
+        <a href="https://www.linkedin.com/in/wai-yan-lin-809998260/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            id="Linkedin-Logo--Streamline-Logos"
+            height="20"
+            width="20"
+          >
+            <desc>Linkedin Logo Streamline Icon: https://streamlinehq.com</desc>
+            <path
+              fill="#000000"
+              fill-rule="evenodd"
+              d="M3.5 6a2.5 2.5 0 1 0 0 -5 2.5 2.5 0 0 0 0 5ZM6 23V8H1v15h5ZM8 8h4.5v1.946C13.216 9.005 14.746 8 17.5 8c4.33 0 5.5 4.32 5.5 7v8h-5v-8c0 -1 -0.5 -3 -2.5 -3 -1.42 0 -2.42 1.008 -3 1.951V23H8V8Z"
+              clip-rule="evenodd"
+              stroke-width="1"
+            ></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
 });
